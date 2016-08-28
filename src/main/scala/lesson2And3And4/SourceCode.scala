4c08445c3eec066c7b84103ba78ee100c8b534cb
notyy <notyycn@gmail.com>
2016-08-28 09:13:57 +0800

    code format

--- src/main/scala/lesson2And3And4/SourceCode.scala
+++ src/main/scala/lesson2And3And4/SourceCode.scala
@@ -1,17 +1,17 @@
 package lesson2And3And4
 
-class SourceCode(val path: String, val name: String, private val lines: List[String]){
-  def count:Int = lines.length
+class SourceCode(val path: String, val name: String, private val lines: List[String]) {
+  def count: Int = lines.length
 }
 
-object SourceCode{
+object SourceCode {
   type Path = String
 
-  def fromFile(path: Path):SourceCode = {
+  def fromFile(path: Path): SourceCode = {
     import scala.io._
 
     val source = Source.fromFile(path)
     val lines = source.getLines.toList
-    new SourceCode(path,path.split("/").last, lines)
+    new SourceCode(path, path.split("/").last, lines)
   }
 }
