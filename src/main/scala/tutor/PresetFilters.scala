b8196e3156723a0b49f6e231f3796f0470a19d59
notyy <notyycn@gmail.com>
2017-01-21 18:53:59 +0800

    ignore .git folder

--- src/main/scala/tutor/PresetFilters.scala
+++ src/main/scala/tutor/PresetFilters.scala
@@ -4,5 +4,5 @@ object PresetFilters {
   val knownFileTypes: Set[String] =
     Set("scala", "java", "txt", "xml", "json", "c", "h", "cpp", "hs", "properties","sbt","js","html")
   val ignoreFolders: Set[String] =
-    Set("target","bin",".idea")
+    Set("target","bin",".idea",".git")
 }
