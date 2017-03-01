c6e848e50f018b4b253b2ddb2bdab912779a55d9
notyy <notyycn@gmail.com>
2017-03-02 00:40:50 +0800

    set functionalTest tag on some tests

--- src/test/scala/tutor/DirectoryScannerSpec.scala
+++ src/test/scala/tutor/DirectoryScannerSpec.scala
@@ -1,14 +1,15 @@
 package tutor
 
 import org.scalatest.{FunSpec, ShouldMatchers}
+import tags.TestTypeTag.FunctionalTest
 import tutor.utils.FileUtil
 
 class DirectoryScannerSpec extends FunSpec with ShouldMatchers {
-  describe("DirectoryScanner"){
+  describe("DirectoryScanner") {
     it("can scan directory recursively and return all file paths" +
-      " and it should only accept known txt files"){
+      " and it should only accept known txt files", FunctionalTest) {
       val ds = new DirectoryScanner {}
-      val files = ds.scan("src/test/fixture", Set("scala","java"), Set("target"))
+      val files = ds.scan("src/test/fixture", Set("scala", "java"), Set("target"))
       files.length shouldBe 2
       FileUtil.extractLocalPath(files.head) shouldBe "SomeCode.scala"
     }
--- src/test/scala/tutor/SourceCodeAnalyzerSpec.scala
+++ src/test/scala/tutor/SourceCodeAnalyzerSpec.scala
@@ -1,10 +1,11 @@
 package tutor
 
 import org.scalatest.{FunSpec, ShouldMatchers}
+import tags.TestTypeTag.FunctionalTest
 
-class SourceCodeAnalyzerSpec extends FunSpec with ShouldMatchers{
-  describe("SourceCode object"){
-    it("can read file and create a SourceCode instance"){
+class SourceCodeAnalyzerSpec extends FunSpec with ShouldMatchers {
+  describe("SourceCode object") {
+    it("can read file and create a SourceCode instance", FunctionalTest) {
       val sca = new SourceCodeAnalyzer {}
       val sourceCodeInfo = sca.processFile("./src/test/fixture/sourceFileSample").get
       sourceCodeInfo.localPath shouldBe "sourceFileSample"
